1dd95f75798139bb7ac214377f1ac3251841380d
mikalep <itstenlayers@gmail.com>
2022-08-28 19:36:07 +0300

    repeatString exercise works with all tests passing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num >= 0) {
+    let str = "";
 
+    for (let i = 0; i < num; i++) {
+      str += string;
+    }
+    return str;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
