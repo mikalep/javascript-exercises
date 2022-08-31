d7fc7efa25b9189f11ef5d4c361c90c9c7fb7fe9
mikalep <itstenlayers@gmail.com>
2022-08-31 23:01:38 +0300

    sumAll exercise works with all tests passing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+  let min   = args[0];
+  let max   = args[args.length-1];
+  let total = 0;
 
+  if (!Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR";
+  }
+  if (min < 0 || max < 0) {
+    return "ERROR";
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  for (let i = min; i < max + 1; i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
