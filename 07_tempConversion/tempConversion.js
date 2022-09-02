0d202feb18df6c302d4609d2df60185e5b62aafa
mikalep <itstenlayers@gmail.com>
2022-09-02 20:30:09 +0300

    tempConversion exercise works with all tests passing

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const roundToOneDecimal = function(value) {
+  return Math.round(value * 10) / 10;
+}
 
+const ftoc = function(f) {
+  let c = (f + 40) / 1.8 - 40;
+  return roundToOneDecimal(c);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = (c + 40) * 1.8 - 40;
+  return roundToOneDecimal(f);
 };
 
 // Do not edit below this line
